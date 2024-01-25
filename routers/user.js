e0e6f0d7faf0cf8ef0b6724df70acee5f43818f0
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2024-01-25 23:56:37 +0530

    Add files via upload

--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,24 @@
 const express = require("express");
-const router = express.Router();
+const Course=require("../models/course")
 const User = require("../models/user");
+const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.render("signup");
+router.get("/signin", (req, res) => {
+  return res.render("signin",{ user: req.user });
 });
+router.get("/signup", (req, res) => {
+  return res.render("signup",{ user: req.user });
+});
+
+router.get("/yourLearnings/:id",async(req,res)=>{
+  
+  const course = await Course.findById(req.params.id)
+  .populate("instructor")
+  .populate("enrolledStudents", "fullName");
+ 
+  const user = await User.findById(req.user?._id)?.populate("enrolledCourses");
+  return res.render("myCourses",{user,course});
+})
 
 router.post("/signup", async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -13,8 +27,26 @@ router.post("/signup", async (req, res) => {
     email,
     password,
   });
-  
   return res.redirect("/");
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const token = await User.matchPasswordAndGenerateToken(email, password);
+
+    console.log("token:", token);
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    console.error(error);
+    return res.render("signin", {
+      error: "Incorrect email or password",
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
 });
 
 module.exports = router;
